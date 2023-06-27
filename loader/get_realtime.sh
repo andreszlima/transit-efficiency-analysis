41ec99de18d3f057cab07e6ac18b4c73a69c76f0
andreszlima <andre.szlima1@gmail.com>
2023-06-26 23:57:34 -0300

    fixed dump/restore and realtime extraction insert

--- loader/get_realtime.sh
+++ loader/get_realtime.sh
@@ -1,54 +1,55 @@
-    #!/bin/bash
+#!/bin/bash
 
-    # Source environment variables
-    source "../scripts/.env"
+# Get the absolute path of the script's directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-    # Remote variables
-    DB_USERNAME=$REMOTE_DB_USERNAME
-    DB_PASSWORD=$REMOTE_DB_PASSWORD
-    DB_NAME=$REMOTE_DB_NAME
-    TABLE_NAME=$REALTIME_TABLE
-    REMOTE_DUMP_FILE_PATH=$VPS_REMOTE_DUMP_FILE_PATH
+# Source environment variables
+source "$SCRIPT_DIR/../scripts/.env"
 
-    # Local variables
-    USERNAME=$VPS_USERNAME
-    SERVER_IP=$VPS_SERVER_IP
-    LOCAL_DUMP_FILE_PATH=$LOCAL_DUMP_FILE_PATH
-    LOCAL_DB_NAME=$LOCAL_DB_NAME
-    PRIVATE_KEY_PATH=$PRIVATE_KEY_PATH
-    LOCAL_USERNAME=$LOCAL_DB_USERNAME
-    LOCAL_PASSWORD=$LOCAL_DB_PASSWORD
+# Remote variables
+DB_USERNAME=$REMOTE_DB_USERNAME
+DB_PASSWORD=$REMOTE_DB_PASSWORD
+DB_NAME=$REMOTE_DB_NAME
+TABLE_NAME=$REALTIME_TABLE
+REMOTE_DUMP_FILE_PATH=$VPS_REMOTE_DUMP_FILE_PATH
 
-    # Create a dump file on the remote server
-    ssh -i $PRIVATE_KEY_PATH $USERNAME@$SERVER_IP "PGPASSWORD=$DB_PASSWORD pg_dump -Fc -U $DB_USERNAME -d $DB_NAME -t $TABLE_NAME -f $REMOTE_DUMP_FILE_PATH" && \
-    echo "Dump file $REMOTE_DUMP_FILE_PATH created for table $TABLE_NAME." || \
-    echo "Failed to create dump file."
+# Local variables
+USERNAME=$VPS_USERNAME
+SERVER_IP=$VPS_SERVER_IP
+LOCAL_DUMP_FILE_PATH=$LOCAL_DUMP_FILE_PATH
+LOCAL_DB_NAME=$LOCAL_DB_NAME
+PRIVATE_KEY_PATH=$PRIVATE_KEY_PATH
+LOCAL_USERNAME=$LOCAL_DB_USERNAME
+LOCAL_PASSWORD=$LOCAL_DB_PASSWORD
 
-    # Transfer the dump file
-    scp -i $PRIVATE_KEY_PATH $USERNAME@$SERVER_IP:$REMOTE_DUMP_FILE_PATH $LOCAL_DUMP_FILE_PATH && \
-    echo "Dump file transferred to local machine." || \
-    echo "Failed to transfer dump file."
+# Create a dump file on the remote server
+ssh -i $PRIVATE_KEY_PATH $USERNAME@$SERVER_IP "PGPASSWORD=$DB_PASSWORD pg_dump -Fc -U $DB_USERNAME -d $DB_NAME -t $TABLE_NAME -f $REMOTE_DUMP_FILE_PATH" && \
+echo "Dump file $REMOTE_DUMP_FILE_PATH created for table $TABLE_NAME." || \
+echo "Failed to create dump file."
 
-    # Connect to local database and drop the existing table
-    PGPASSWORD=$LOCAL_PASSWORD psql -U $LOCAL_USERNAME -d $LOCAL_DB_NAME -h localhost << EOF
+# Transfer the dump file
+scp -i $PRIVATE_KEY_PATH $USERNAME@$SERVER_IP:$REMOTE_DUMP_FILE_PATH $LOCAL_DUMP_FILE_PATH && \
+echo "Dump file transferred to local machine." || \
+echo "Failed to transfer dump file."
 
-    DROP TABLE IF EXISTS $TABLE_NAME;
+# Connect to local database and drop the existing table
+PGPASSWORD=$LOCAL_PASSWORD psql -U $LOCAL_USERNAME -d $LOCAL_DB_NAME -h localhost <<EOF
+DROP TABLE IF EXISTS $TABLE_NAME;
+EOF
 
-    EOF
+echo "Existing table dropped."
 
-    echo "Existing table dropped."
+# Restore the table from the dump file
+PGPASSWORD=$LOCAL_PASSWORD pg_restore -U $LOCAL_USERNAME -d $LOCAL_DB_NAME -v -F c $LOCAL_DUMP_FILE_PATH && \
+echo "Table $TABLE_NAME restored from $LOCAL_DUMP_FILE_PATH." || \
+echo "Failed to restore table."
 
-    # Restore the table from the dump file
-    PGPASSWORD=$LOCAL_PASSWORD pg_restore -U $LOCAL_USERNAME -d $LOCAL_DB_NAME -v -F c -t $TABLE_NAME $LOCAL_DUMP_FILE_PATH && \
-    echo "Table $TABLE_NAME restored from $LOCAL_DUMP_FILE_PATH." || \
-    echo "Failed to restore table."
+# Remove local dump file
+rm $LOCAL_DUMP_FILE_PATH && \
+echo "Local dump file removed." || \
+echo "Failed to remove local dump file."
 
-    # Remove local dump file
-    rm $LOCAL_DUMP_FILE_PATH && \
-    echo "Local dump file removed." || \
-    echo "Failed to remove local dump file."
-
-    # Remove remote dump file
-    ssh -i $PRIVATE_KEY_PATH $USERNAME@$SERVER_IP "rm $REMOTE_DUMP_FILE_PATH" && \
-    echo "Remote dump file removed." || \
-    echo "Failed to remove remote dump file."
+# Remove remote dump file
+ssh -i $PRIVATE_KEY_PATH $USERNAME@$SERVER_IP "rm $REMOTE_DUMP_FILE_PATH" && \
+echo "Remote dump file removed." || \
+echo "Failed to remove remote dump file."
--- scripts/realtime_extractor.py
+++ scripts/realtime_extractor.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import pytz
 import requests
+from datetime import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 import sys
@@ -21,6 +22,8 @@ db_string = os.getenv("REMOTE_DB_URL")
 # The table name can be replaced
 table_name = os.getenv("REALTIME_TABLE")
 
+zero_timestamp = pd.Timestamp('1970-01-01', tz='America/Toronto')
+
 def parse_pb_data(data):
     feed = gtfs_realtime_pb2.FeedMessage()
     feed.ParseFromString(data)
@@ -30,8 +33,8 @@ def parse_pb_data(data):
         if entity.HasField('trip_update'):
             trip_id = entity.trip_update.trip.trip_id
             for update in entity.trip_update.stop_time_update:
-                departure_time = pd.to_datetime(update.departure.time, unit='s', utc=True).tz_convert('America/Toronto') if update.HasField('departure') else pd.Timestamp('1970-01-01', tz='America/Toronto')
-                arrival_time = pd.to_datetime(update.arrival.time, unit='s', utc=True).tz_convert('America/Toronto') if update.HasField('arrival') else pd.Timestamp('1970-01-01', tz='America/Toronto')
+                departure_time = pd.to_datetime(update.departure.time, unit='s', utc=True).tz_convert('America/Toronto') if update.HasField('departure') else zero_timestamp
+                arrival_time = pd.to_datetime(update.arrival.time, unit='s', utc=True).tz_convert('America/Toronto') if update.HasField('arrival') else zero_timestamp
                 parsed_data.append({
                     'trip_id': trip_id,
                     'stop_sequence': update.stop_sequence,
@@ -70,17 +73,25 @@ def main():
     print(f'Inserting data with source {timestamp}...')
     df['file_source'] = timestamp
 
+    # Get current time
+    now = datetime.now(pytz.timezone('America/Toronto'))
+
     # Insert data into the database
     try:
         with engine.connect() as conn:
             for _, row in df.iterrows():
-                insert_query = text(f"""
-                    INSERT INTO {table_name} (trip_id, stop_sequence, stop_id, departure_time, arrival_time, file_source)
-                    VALUES (:trip_id, :stop_sequence, :stop_id, :departure_time, :arrival_time, :file_source)
-                    ON CONFLICT (trip_id, stop_sequence, stop_id, departure_time, arrival_time) 
-                    DO NOTHING
-                """)
-                conn.execute(insert_query, row.to_dict())
+                # Check if arrival or departure time is older than the current time
+                is_departure_valid = row['departure_time'].to_pydatetime() != zero_timestamp and row['departure_time'].to_pydatetime() < now
+                is_arrival_valid = row['arrival_time'].to_pydatetime() != zero_timestamp and row['arrival_time'].to_pydatetime() < now
+
+                if is_departure_valid or is_arrival_valid:
+                    insert_query = text(f"""
+                        INSERT INTO {table_name} (trip_id, stop_sequence, stop_id, departure_time, arrival_time, file_source)
+                        VALUES (:trip_id, :stop_sequence, :stop_id, :departure_time, :arrival_time, :file_source)
+                        ON CONFLICT (trip_id, stop_sequence, stop_id, departure_time, arrival_time) 
+                        DO NOTHING
+                    """)
+                    conn.execute(insert_query, row.to_dict())
             conn.commit()
         print('Data inserted.')
     except Exception as e:
