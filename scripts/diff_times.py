58b41a4d7e4c76a9f5abc126fc27c805a7602792
andreszlima <andre.szlima1@gmail.com>
2023-07-03 23:29:43 -0300

    fixed timezone for hour of day

--- scripts/diff_times.py
+++ scripts/diff_times.py
@@ -31,17 +31,33 @@ def populate_table():
         gd.stop_name, 
         gd.route_long_name, 
         CASE
-            WHEN EXTRACT(EPOCH FROM tu.arrival_time) = 0 THEN gd.arrival_time
+            WHEN EXTRACT(EPOCH FROM tu.arrival_time) = 0 THEN NULL
             ELSE tu.arrival_time
         END AS actual_arrival_time, 
         gd.arrival_time AS scheduled_arrival_time,
-        EXTRACT(EPOCH FROM ((CASE WHEN EXTRACT(EPOCH FROM tu.arrival_time) = 0 THEN gd.arrival_time ELSE tu.arrival_time END) - gd.arrival_time)) / 60 AS arrival_time_diff_in_minutes,
         CASE
-            WHEN EXTRACT(EPOCH FROM tu.departure_time) = 0 THEN gd.departure_time
+            WHEN EXTRACT(EPOCH FROM tu.arrival_time) = 0 THEN 0
+            ELSE EXTRACT(EPOCH FROM (tu.arrival_time - gd.arrival_time)) / 60
+        END AS arrival_time_diff_in_minutes,
+        CASE
+            WHEN EXTRACT(EPOCH FROM tu.departure_time) = 0 THEN NULL
             ELSE tu.departure_time
         END AS actual_departure_time,
         gd.departure_time AS scheduled_departure_time,
-        EXTRACT(EPOCH FROM ((CASE WHEN EXTRACT(EPOCH FROM tu.departure_time) = 0 THEN gd.departure_time ELSE tu.departure_time END) - gd.departure_time)) / 60 AS departure_time_diff_in_minutes,
+        CASE
+            WHEN EXTRACT(EPOCH FROM tu.departure_time) = 0 THEN 0
+            ELSE EXTRACT(EPOCH FROM (tu.departure_time - gd.departure_time)) / 60
+        END AS departure_time_diff_in_minutes,
+        CASE
+            WHEN (EXTRACT(EPOCH FROM tu.arrival_time) <> 0 AND EXTRACT(EPOCH FROM tu.departure_time) <> 0) THEN
+                (EXTRACT(EPOCH FROM (tu.arrival_time - gd.arrival_time)) + EXTRACT(EPOCH FROM (tu.departure_time - gd.departure_time))) / 120
+            WHEN (EXTRACT(EPOCH FROM tu.arrival_time) = 0 AND EXTRACT(EPOCH FROM tu.departure_time) <> 0) THEN
+                EXTRACT(EPOCH FROM (tu.departure_time - gd.departure_time)) / 60
+            WHEN (EXTRACT(EPOCH FROM tu.arrival_time) <> 0 AND EXTRACT(EPOCH FROM tu.departure_time) = 0) THEN
+                EXTRACT(EPOCH FROM (tu.arrival_time - gd.arrival_time)) / 60
+            ELSE
+                NULL
+        END AS average_diff_in_minutes,
         tu.weather_group,
         tu.weather_description,
         CASE
@@ -53,7 +69,7 @@ def populate_table():
             WHEN EXTRACT(DOW FROM gd.arrival_time AT TIME ZONE 'UTC' AT TIME ZONE 'America/Toronto') = 5 THEN 'Friday'
             WHEN EXTRACT(DOW FROM gd.arrival_time AT TIME ZONE 'UTC' AT TIME ZONE 'America/Toronto') = 6 THEN 'Saturday'
         END AS day_type,
-        DATE_PART('hour', gd.arrival_time AT TIME ZONE 'UTC' AT TIME ZONE 'America/Toronto') AS hour_of_day,
+        DATE_PART('hour', gd.arrival_time AT TIME ZONE 'America/Toronto') AS sudbury_hour_of_day,
         gd.geo_coordinates,
         tu.created_at,
         tu.updated_at
@@ -66,8 +82,8 @@ def populate_table():
         AND tu.stop_id::bigint = gd.stop_id 
     WHERE 
         NOT (
-            EXTRACT(EPOCH FROM ((CASE WHEN EXTRACT(EPOCH FROM tu.arrival_time) = 0 THEN gd.arrival_time ELSE tu.arrival_time END) - gd.arrival_time)) = 0 AND
-            EXTRACT(EPOCH FROM ((CASE WHEN EXTRACT(EPOCH FROM tu.departure_time) = 0 THEN gd.departure_time ELSE tu.departure_time END) - gd.departure_time)) = 0
+            (EXTRACT(EPOCH FROM tu.arrival_time) = 0 AND EXTRACT(EPOCH FROM gd.arrival_time) <= 1000 * 60) AND
+            (EXTRACT(EPOCH FROM tu.departure_time) = 0 AND EXTRACT(EPOCH FROM gd.departure_time) <= 1000 * 60)
         )
     ORDER BY tu.trip_id ASC, tu.stop_sequence ASC, tu.start_date ASC;
     """
